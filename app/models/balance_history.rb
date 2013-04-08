8f51070c62e1ec32caeb5aff3708b71ef1128f88
Piotr Imbierowicz <piotr@k164-2.dwudziestolatka.ds.uni.wroc.pl>
2013-04-08 08:32:52 +0200

    Sign count admin

--- app/models/balance_history.rb
+++ app/models/balance_history.rb
@@ -12,7 +12,7 @@ class BalanceHistory < ActiveRecord::Base
 
   def reversed_value
     if self[:value] == 0
-      return 0
+      return format('%.2f PLN', 0.0)
     end
     balance = self[:value].to_f / -100
     format('%.2f PLN', balance)
